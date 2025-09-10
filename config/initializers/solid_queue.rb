592dd61f71a2e507f4131e2f4622a989e4d57b96
senthil-ragavan <68710233+senthil-ragavan@users.noreply.github.com>
2025-09-10 11:51:48 +0530

    Update solid_queue.rb

--- config/initializers/solid_queue.rb
+++ config/initializers/solid_queue.rb
@@ -1,3 +1,5 @@
-SolidQueue.configure do |config|
-  config.database = Rails.env.production? ? :queue : nil
+if SolidQueue.respond_to?(:configure)
+  SolidQueue.configure do |config|
+    config.database = Rails.env.production? ? :queue : nil
+  end
 end
